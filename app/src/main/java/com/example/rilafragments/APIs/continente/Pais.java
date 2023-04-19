2e6dafdc98c485f478c9eefd01079c7c41dc191b
Hugo <hugome2307@gmail.com>
2023-04-19 19:24:44 +0200

    b

--- app/src/main/java/com/example/rilafragments/APIs/continente/Pais.java
+++ app/src/main/java/com/example/rilafragments/APIs/continente/Pais.java
@@ -1,9 +1,13 @@
 package com.example.rilafragments.APIs.continente;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pais {
 	private String bandera;
+	private String id;
 	private String nombre;
-	private String[] ciudades;
+	private ArrayList<CiudadesItem> ciudades;
 
 	public void setBandera(String bandera){
 		this.bandera = bandera;
@@ -11,6 +15,14 @@ public class Pais {
 
 	public String getBandera(){
 		return bandera;
+	}
+
+	public void setId(String id){
+		this.id = id;
+	}
+
+	public String getId(){
+		return id;
 	}
 
 	public void setNombre(String nombre){
@@ -21,16 +33,22 @@ public class Pais {
 		return nombre;
 	}
 
-	public void setCiudades(String[] ciudades) {this.ciudades = ciudades;}
+	public void setCiudades(ArrayList<CiudadesItem> ciudades){
+		this.ciudades = ciudades;
+	}
 
-	public String[] getCiudades() {return ciudades;}
+	public ArrayList<CiudadesItem> getCiudades(){
+		return ciudades;
+	}
 
 	@Override
  	public String toString(){
 		return 
 			"PaisesItem{" + 
 			"bandera = '" + bandera + '\'' + 
+			",_id = '" + id + '\'' + 
 			",nombre = '" + nombre + '\'' + 
+			",ciudades = '" + ciudades + '\'' + 
 			"}";
 		}
 }
